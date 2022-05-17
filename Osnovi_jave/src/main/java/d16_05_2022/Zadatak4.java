25dfdb5c8aaf2778584174093b5af2a841f1d0b7
Marija Djuric <mdjuric034@gmail.com>
2022-05-17 06:38:23 +0200

    Domaći 16.05.2022. Zadatak 4

---
+++ Osnovi_jave/src/main/java/d16_05_2022/Zadatak4.java
@@ -0,0 +1,15 @@
+package d16_05_2022;
+
+public class Zadatak4 {
+    //Metoda prima dva parametra N i karakter za stampu. Metoda stampa N karaktera.
+    //Ako se za N prosledi 5 i za karakter prosledi npr kosa crta (/) stampa se=>
+    /// / / / /
+    public static void main(String[] args) {
+        štampaSe(5,"/");
+    }
+    static void štampaSe (int n, String karakter) {
+        for (int i=0; i<n; i++) {
+            System.out.print(karakter);
+        }
+    }
+}
